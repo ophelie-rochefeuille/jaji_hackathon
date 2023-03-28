7c9b70c8419a71fc7d91c6c99faad7ec37d8c6b8
Ophélie Rochefeuille <ophelie.rochefeuille@green-conseil.com>
2023-03-28 18:25:48 +0200

    test front vue

---
+++ src/components/menus/SideBar.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <h1>TEST</h1>
+  <p>{{test}}</p>
+
+  {{title}}
+
+
+
+</template>
+<script>
+
+export default {
+  components: {},
+  props: {
+    title: {
+      type: String
+    },
+    array: []
+  },
+  methods: {
+  },
+  data() {
+    return {
+      test: "test",
+    };
+  },
+}
+
+</script>
+<style lang="css" scoped>
+
+</style>
